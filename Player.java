b34d957da1bda4db9dd397e9e017289f2217f101
Hud <hhw3103@hotmail.com>
2020-11-03 12:17:07 +0100

    PlayerUdkast1

--- Player.java
+++ Player.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+
 public class Player {
+    private ArrayList <Trash> inventoryList;
+
+
+    //Her har vi vores metoder:
+    public void openInventory (){
+
+    }
+
+    public void inspectTrash (){ //MANGLER INPUT ER DET TRASH ELLER INT?
+
+    }
+    public void pickUp (Trash trash){
+
+    }
 }
