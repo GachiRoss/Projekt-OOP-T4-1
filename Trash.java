a989a07052aed80992d9674cbdab23876c717592
Emma <em.kirstine@gmail.com>
2020-11-03 12:51:27 +0100

    hej

--- Trash.java
+++ Trash.java
@@ -1,7 +1,15 @@
 public class Trash {
+    // Attributter:
     public String material;
     public String name;
-    public int numberID;
+    public int ID;
 
+    // Constructor: objects (trash) are made here
+    Trash (String material, String name, int trashID){
+        this.material = material;
+        this.name = name;
+        this.ID = trashID;
+
+    }
 
 }
