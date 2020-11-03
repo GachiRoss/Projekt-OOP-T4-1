106a3e79e8ba318e0d41a1d153a4bf0ccd76e5a2
Flora <betur20@student.sdu.dk>
2020-11-03 14:30:54 +0100

    Room og Trash Arraylist og trash tilføjet

--- Room.java
+++ Room.java
@@ -1,18 +1,31 @@
 import java.util.Set;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.ArrayList;
 
 
 public class Room 
 {
     private String description;
-    public String[] trash;
+    public ArrayList<Trash> trash = new ArrayList<Trash>(5);
     private HashMap<String, Room> exits; //?
 
-    public Room(String description) 
+    private Trash can = new Trash("Metal", "Can", 1);
+    private Trash battery = new Trash("Hazardous waste", "Battery", 2);
+    private Trash pizzaBox = new Trash("Residual waste", "Pizza box", 3);
+    private Trash plasticBag = new Trash("Plastic", "Plastic bag", 4);
+    private Trash porcelainPlate = new Trash("Hazardous waste", "Pocelain plate", 5);
+
+    public Room(String description, ArrayList<Trash> trash)
     {
         this.description = description;
         exits = new HashMap<String, Room>();
+
+        trash.add(battery);
+        trash.add(can);
+        trash.add(pizzaBox);
+        trash.add(plasticBag);
+        trash.add(porcelainPlate);
     }
 
     public void setExit(String direction, Room neighbor) 
--- Trash.java
+++ Trash.java
@@ -1,7 +1,13 @@
 public class Trash {
     public String material;
     public String name;
-    public int numberID;
+    public int ID;
+
+    Trash(String material, String name, int trashID){
+        this.material = material;
+        this.name = name;
+        this.ID = trashID;
+    }
 
 
 }
