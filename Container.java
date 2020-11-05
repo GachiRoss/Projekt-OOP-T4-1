63285e3e5e1b8c0c2acdf4e6ed3b65ab444572c4
Tobias <Tobber00@gmail.com>
2020-11-05 10:19:00 +0100

    hej

--- Container.java
+++ Container.java
@@ -5,16 +5,17 @@ public class Container {
 
     public Container(String containerName, int containerID) {
         ID = containerID;
-        name = containerName;   //there might be a problem with the name variable here and the one in the trash class i think.
-        // Fairly sure there are two variables called "name"
+        name = containerName;
     }
-
-    //trashID is a placeholder name. It might change if Flora and Emma decide to use another name in their classes
-    public boolean checkRecycling(int trashID, int containerID) {
-        if (trashID == containerID)
-            return true;
-        else {
-            return false;
+    public int checkRecycling(Trash trash) {
+        if (trash != null) {
+            if (ID == trash.getID()) {
+                return 1;
+            } else {
+                return -1;
+            }
         }
+        return 0;
+
     }
 }
